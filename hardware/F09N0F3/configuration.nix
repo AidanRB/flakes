fb1eddd0fb1e780c4bd6396110875cd0f229e413
AidanRB <aidanrichbennett@gmail.com>
2025-01-21 12:17:39 -0500

    add UEFI to F09N0F3

--- hardware/F09N0F3/configuration.nix
+++ hardware/F09N0F3/configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../UEFI.nix
     ];
 
   networking.hostName = "F09N0F3"; # Define your hostname.
