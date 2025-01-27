63cd2d8d95c2ebf117d4891b0c2fc1ea3bd2339c
AidanRB <github@linux.contact>
2025-01-26 20:02:47 -0500

    switch to ghostty

--- users/aidan.nix
+++ users/aidan.nix
@@ -42,6 +42,96 @@ in
   };
 
   home-manager.users.aidan = {
+    programs = {
+      fish = {
+        enable = true;
+
+        functions = {
+          nix-shell = "command nix-shell --run fish $argv";
+
+          ls = "command ls --icons $argv";
+
+          fish_prompt = {
+            body = ''
+              set -l last_pipestatus $pipestatus
+              set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+              set -l normal (set_color normal)
+              set -q fish_color_status
+              or set -g fish_color_status --background=red white
+
+              # Color the prompt differently when we're root
+              set -l color_cwd $fish_color_cwd
+              set -l suffix '>'
+              if functions -q fish_is_root_user; and fish_is_root_user
+                  if set -q fish_color_cwd_root
+                      set color_cwd $fish_color_cwd_root
+                  end
+                  set suffix '#'
+              end
+
+              # Write pipestatus
+              # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+              set -l bold_flag --bold
+              set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+              if test $__fish_prompt_status_generation = $status_generation
+                  set bold_flag
+              end
+              set __fish_prompt_status_generation $status_generation
+              set -l status_color (set_color $fish_color_status)
+              set -l statusb_color (set_color $bold_flag $fish_color_status)
+              set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+              # add nix-shell info
+              if test -n "$IN_NIX_SHELL"
+                set_color bryellow
+                echo -n "<nix-shell> "
+              end
+
+              echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd --full-length-dirs 1) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+            '';
+          };
+        };
+      };
+
+      ghostty = {
+        enable = true;
+        enableFishIntegration = true;
+        settings = {
+          theme = "kgx";
+          font-family = "Source Code Pro";
+          font-size = 10;
+          window-padding-x = 6;
+          gtk-single-instance = true;
+        };
+        themes = {
+          kgx = {
+            background = "1f1f1f";
+            cursor-color = "ffffff";
+            foreground = "ffffff";
+            palette = [
+              "0=#241f31"
+              "1=#c01c28"
+              "2=#2ec27e"
+              "3=#f5c211"
+              "4=#1e78e4"
+              "5=#9841bb"
+              "6=#0ab9dc"
+              "7=#c0bfbc"
+              "8=#5e5c64"
+              "9=#ed333b"
+              "10=#57e389"
+              "11=#f8e45c"
+              "12=#51a1ff"
+              "13=#c061cb"
+              "14=#4fd2fd"
+              "15=#f6f5f4"
+            ];
+            selection-invert-fg-bg = true;
+          };
+        };
+      };
+    };
+
     gtk = {
       enable = true;
 
@@ -53,6 +143,10 @@ in
 
     home = {
       keyboard.options = [ "compose:caps" ];
+
+      sessionVariables = {
+        fish_greeting = "";
+      };
 
       stateVersion = "24.11";
     };
@@ -80,8 +174,8 @@ in
 
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         binding = "<Super>t";
-        command = "kgx";
-        name = "console";
+        command = "ghostty";
+        name = "terminal";
       };
 
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
