e1f4c77301d113670b8ad418ecf80f5f61f8d693
rtbennett <rtbennett64@gmail.com>
2025-01-28 11:18:35 -0500

    added home-maneger

--- users/reuben.nix
+++ users/reuben.nix
@@ -21,4 +21,42 @@
 
   programs.fish.enable = true;
   programs.steam.enable = true;
+
+    home-manager.users.reuben = {
+    gtk = {
+      enable = true;
+      cursorTheme = {
+        name = "Bibata-Modern-Classic";
+        package = pkgs.bibata-cursors;
+      };
+    };
+    home = {
+      keyboard.options = [ "compose:caps" ];
+      stateVersion = "24.11";
+    };
+    dconf.settings = {
+      # Pinned apps
+      "org/gnome/shell" = {
+        favorite-apps = [
+          "brave-browser.desktop"
+          "org.gnome.Nautilus.desktop"
+          "discord.desktop"
+          "steam.desktop"
+        ];
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        binding = "<Super>t";
+        command = "kgx";
+        name = "console";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+        binding = "Print";
+        command = "bash -c \"flameshot gui > /dev/null\"";
+        name = "flameshot";
+      };
+    };  
+  };  
+
 }
