4889979378f98b4c550a3c81f0ca6634ce60321f
AidanRB <aidanrichbennett@gmail.com>
2025-01-27 20:05:29 -0500

    home-manager allow overriding existing files

--- users/aidan.nix
+++ users/aidan.nix
@@ -48,161 +48,165 @@ in
     };
   };
 
-  home-manager.users.aidan = {
-    programs = {
-      fish = {
-        enable = true;
+  home-manager = {
+    backupFileExtension = ".old";
 
-        functions = {
-          nix-shell = "command nix-shell --run fish $argv";
+    users.aidan = {
+      programs = {
+        fish = {
+          enable = true;
 
-          ls = "command ls --icons $argv";
+          functions = {
+            nix-shell = "command nix-shell --run fish $argv";
 
-          fish_prompt = {
-            body = ''
-              set -l last_pipestatus $pipestatus
-              set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-              set -l normal (set_color normal)
-              set -q fish_color_status
-              or set -g fish_color_status --background=red white
+            ls = "command ls --icons $argv";
 
-              # Color the prompt differently when we're root
-              set -l color_cwd $fish_color_cwd
-              set -l suffix '>'
-              if functions -q fish_is_root_user; and fish_is_root_user
-                  if set -q fish_color_cwd_root
-                      set color_cwd $fish_color_cwd_root
-                  end
-                  set suffix '#'
-              end
+            fish_prompt = {
+              body = ''
+                set -l last_pipestatus $pipestatus
+                set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+                set -l normal (set_color normal)
+                set -q fish_color_status
+                or set -g fish_color_status --background=red white
 
-              # Write pipestatus
-              # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
-              set -l bold_flag --bold
-              set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
-              if test $__fish_prompt_status_generation = $status_generation
-                  set bold_flag
-              end
-              set __fish_prompt_status_generation $status_generation
-              set -l status_color (set_color $fish_color_status)
-              set -l statusb_color (set_color $bold_flag $fish_color_status)
-              set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+                # Color the prompt differently when we're root
+                set -l color_cwd $fish_color_cwd
+                set -l suffix '>'
+                if functions -q fish_is_root_user; and fish_is_root_user
+                    if set -q fish_color_cwd_root
+                        set color_cwd $fish_color_cwd_root
+                    end
+                    set suffix '#'
+                end
 
-              # add nix-shell info
-              if test -n "$IN_NIX_SHELL"
-                set_color bryellow
-                echo -n "<nix-shell> "
-              end
+                # Write pipestatus
+                # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+                set -l bold_flag --bold
+                set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+                if test $__fish_prompt_status_generation = $status_generation
+                    set bold_flag
+                end
+                set __fish_prompt_status_generation $status_generation
+                set -l status_color (set_color $fish_color_status)
+                set -l statusb_color (set_color $bold_flag $fish_color_status)
+                set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-              echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd --full-length-dirs 1) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
-            '';
+                # add nix-shell info
+                if test -n "$IN_NIX_SHELL"
+                  set_color bryellow
+                  echo -n "<nix-shell> "
+                end
+
+                echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd --full-length-dirs 1) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+              '';
+            };
+          };
+        };
+
+        ghostty = {
+          enable = true;
+          enableFishIntegration = true;
+          settings = {
+            theme = "kgx";
+            font-family = "Source Code Pro";
+            font-size = 10;
+            window-padding-x = 6;
+            gtk-single-instance = true;
+          };
+          themes = {
+            kgx = {
+              background = "1f1f1f";
+              cursor-color = "ffffff";
+              foreground = "ffffff";
+              palette = [
+                "0=#241f31"
+                "1=#c01c28"
+                "2=#2ec27e"
+                "3=#f5c211"
+                "4=#1e78e4"
+                "5=#9841bb"
+                "6=#0ab9dc"
+                "7=#c0bfbc"
+                "8=#5e5c64"
+                "9=#ed333b"
+                "10=#57e389"
+                "11=#f8e45c"
+                "12=#51a1ff"
+                "13=#c061cb"
+                "14=#4fd2fd"
+                "15=#f6f5f4"
+              ];
+              selection-invert-fg-bg = true;
+            };
           };
         };
       };
 
-      ghostty = {
+      gtk = {
         enable = true;
-        enableFishIntegration = true;
-        settings = {
-          theme = "kgx";
-          font-family = "Source Code Pro";
-          font-size = 10;
-          window-padding-x = 6;
-          gtk-single-instance = true;
-        };
-        themes = {
-          kgx = {
-            background = "1f1f1f";
-            cursor-color = "ffffff";
-            foreground = "ffffff";
-            palette = [
-              "0=#241f31"
-              "1=#c01c28"
-              "2=#2ec27e"
-              "3=#f5c211"
-              "4=#1e78e4"
-              "5=#9841bb"
-              "6=#0ab9dc"
-              "7=#c0bfbc"
-              "8=#5e5c64"
-              "9=#ed333b"
-              "10=#57e389"
-              "11=#f8e45c"
-              "12=#51a1ff"
-              "13=#c061cb"
-              "14=#4fd2fd"
-              "15=#f6f5f4"
-            ];
-            selection-invert-fg-bg = true;
-          };
+
+        cursorTheme = {
+          name = "Bibata-Modern-Classic";
+          package = pkgs.bibata-cursors;
         };
       };
-    };
 
-    gtk = {
-      enable = true;
+      home = {
+        keyboard.options = [ "compose:caps" ];
 
-      cursorTheme = {
-        name = "Bibata-Modern-Classic";
-        package = pkgs.bibata-cursors;
-      };
-    };
+        sessionVariables = {
+          EDITOR = "micro";
+          VISUAL = "micro";
+          fish_greeting = "";
+        };
 
-    home = {
-      keyboard.options = [ "compose:caps" ];
-
-      sessionVariables = {
-        EDITOR = "micro";
-        VISUAL = "micro";
-        fish_greeting = "";
+        stateVersion = "24.11";
       };
 
-      stateVersion = "24.11";
-    };
+      dconf.settings = {
+        # Pinned apps
+        "org/gnome/shell" = {
+          favorite-apps = [
+            "brave-browser.desktop"
+            "org.gnome.Nautilus.desktop"
+            "discord.desktop"
+            "steam.desktop"
+          ];
+        };
 
-    dconf.settings = {
-      # Pinned apps
-      "org/gnome/shell" = {
-        favorite-apps = [
-          "brave-browser.desktop"
-          "org.gnome.Nautilus.desktop"
-          "discord.desktop"
-          "steam.desktop"
-        ];
-      };
+        # Custom keybinds
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          custom-keybindings = [
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+          ];
+        };
 
-      # Custom keybinds
-      "org/gnome/settings-daemon/plugins/media-keys" = {
-        custom-keybindings = [
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
-        ];
-      };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "<Super>t";
+          command = "ghostty";
+          name = "terminal";
+        };
 
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        binding = "<Super>t";
-        command = "ghostty";
-        name = "terminal";
-      };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+          binding = "Print";
+          command = "bash -c \"flameshot gui > /dev/null\"";
+          name = "flameshot";
+        };
 
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-        binding = "Print";
-        command = "bash -c \"flameshot gui > /dev/null\"";
-        name = "flameshot";
-      };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+          binding = "<Super>c";
+          command = "speedcrunch";
+          name = "speedcrunch";
+        };
 
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
-        binding = "<Super>c";
-        command = "speedcrunch";
-        name = "speedcrunch";
-      };
-
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
-        binding = "<Super>d";
-        command = "${toggleDND}";
-        name = "toggle_dnd";
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+          binding = "<Super>d";
+          command = "${toggleDND}";
+          name = "toggle_dnd";
+        };
       };
     };
   };
