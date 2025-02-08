b54611c96ff8afa680f943dea30be912e2e7b4e5
rtbennett <189423243+rtbennett@users.noreply.github.com>
2025-02-08 16:35:04 -0500

    fix home-manager for reuben

--- users/reuben.nix
+++ users/reuben.nix
@@ -15,7 +15,6 @@
       brave
       discord
       vscode-fhs
-      bibata-cursors
       flameshot
       # blender
       sshfs
@@ -29,39 +28,43 @@
   programs.fish.enable = true;
   programs.steam.enable = true;
 
-  home-manager.users.reuben = {
-    gtk = {
-      enable = true;
-      cursorTheme = {
-        name = "Bibata-Modern-Classic";
-        package = pkgs.bibata-cursors;
-      };
-    };
-    home = {
-      keyboard.options = [ "compose:caps" ];
-      stateVersion = "24.11";
-    };
-    dconf.settings = {
-      # Pinned apps
-      "org/gnome/shell" = {
-        favorite-apps = [
-          "brave-browser.desktop"
-          "org.gnome.Nautilus.desktop"
-          "discord.desktop"
-          "steam.desktop"
-        ];
-      };
+  home-manager = {
+    backupFileExtension = ".old";
 
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        binding = "<Super>t";
-        command = "kgx";
-        name = "console";
+    users.reuben = {
+      gtk = {
+        enable = true;
+        cursorTheme = {
+          name = "Bibata-Modern-Classic";
+          package = pkgs.bibata-cursors;
+        };
       };
+      home = {
+        keyboard.options = [ "compose:caps" ];
+        stateVersion = "24.11";
+      };
+      dconf.settings = {
+        # Pinned apps
+        "org/gnome/shell" = {
+          favorite-apps = [
+            "brave-browser.desktop"
+            "org.gnome.Nautilus.desktop"
+            "discord.desktop"
+            "steam.desktop"
+          ];
+        };
 
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-        binding = "Print";
-        command = "bash -c \"flameshot gui > /dev/null\"";
-        name = "flameshot";
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "<Super>t";
+          command = "kgx";
+          name = "console";
+        };
+
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+          binding = "Print";
+          command = "bash -c \"flameshot gui > /dev/null\"";
+          name = "flameshot";
+        };
       };
     };
   };
