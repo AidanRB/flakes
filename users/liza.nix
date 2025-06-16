34ffdcb6589c47aa604767b37d3781194b41bf07
AidanRB <aidanrichbennett@gmail.com>
2025-06-16 17:14:27 -0400

    add password to liza

--- users/liza.nix
+++ users/liza.nix
@@ -4,6 +4,7 @@
   users.users.liza = {
     isNormalUser = true;
     description = "Elizabeth Bennett";
+    hashedPassword = "$y$j9T$FEpQnkY0kxAo4GeuJxv5L1$Pmi/Xe6f7zBKzE4Gb1LOnCaW0FMJZE8.k8IDDd3qSg1";
     extraGroups = [
       "networkmanager"
     ];
