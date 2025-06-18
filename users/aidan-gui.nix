d00507d720e3f36cf2faea8529891d5621d4e898
AidanRB <aidanrichbennett@gmail.com>
2025-06-18 17:44:29 -0400

    remove gnome ssh workaround that didn't work anyway

--- users/aidan-gui.nix
+++ users/aidan-gui.nix
@@ -24,13 +24,6 @@ let
     hash = "sha256-qFD7FFqY2l2Fsev/YPu1/f7f29BntxsO1ezJj2BlMs8=";
   };
 
-  stopGnomeSsh = pkgs.writeTextFile {
-    name = "gnome-keyring-ssh.desktop";
-    text = (builtins.readFile (pkgs.gnome-keyring + "/etc/xdg/autostart/gnome-keyring-ssh.desktop")) + ''
-      Hidden=true
-    '';
-  };
-
 in
 
 {
@@ -237,7 +230,6 @@ in
         enable = true;
         entries = [
           "${pkgs.bitwarden}/share/applications/bitwarden.desktop"
-          stopGnomeSsh
         ];
       };
     };
