5b8a15b785cee83ffcc13cd560835e274bf9ec42
AidanRB <aidanrichbennett@gmail.com>
2025-11-21 07:01:30 -0500

    more hyprland

--- users/aidan-hyprland.nix
+++ users/aidan-hyprland.nix
@@ -8,9 +8,20 @@
 
   users.users.aidan.packages = with pkgs; [
     wofi
+    playerctl
   ];
 
   programs.waybar.enable = true;
 
+  fonts.packages = with pkgs; [
+    font-awesome
+  ];
+
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  home-manager.users.aidan = {
+    wayland.windowManager.hyprland.plugins = with pkgs; [
+      hyprlandPlugins.hyprexpo
+    ];
+  };
 }
