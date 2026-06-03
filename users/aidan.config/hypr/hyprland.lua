64f3a385328e385bde6b05acb8f4367b6d2b3cf6
AidanRB <aidanrichbennett@gmail.com>
2026-06-03 17:20:41 -0400

    fix/add hyprland keybinds

--- users/aidan.config/hypr/hyprland.lua
+++ users/aidan.config/hypr/hyprland.lua
@@ -281,10 +281,10 @@ hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO
 })
 hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"))
 hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"))
-hl.bind("SUPER + XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"))
-hl.bind("SUPER + XF86AudioNext", hl.dsp.exec_cmd("playerctl next"))
-hl.bind("SUPER + XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"))
-hl.bind("SUPER + XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"))
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"))
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"))
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"))
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"))
 -- these don't work on a desktop, need to replace
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"))
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"))
@@ -306,6 +306,7 @@ hl.bind("CTRL + ALT + DELETE", hl.dsp.exec_cmd("walker --provider menus power"))
 hl.bind("PRINT", hl.dsp.exec_cmd("XDG_CURRENT_DESKTOP=sway flameshot gui"))
 hl.bind("SUPER + PRINT", hl.dsp.exec_cmd("pkill -SIGUSR1 -f gpu-screen-recorder"))
 hl.bind("SUPER + f", hl.dsp.exec_cmd("nautilus"))
+hl.bind("XF86Calculator", hl.dsp.exec_cmd("speedcrunch"))
 -- log out
 hl.bind("SUPER + m", hl.dsp.exec_cmd("uwsm stop"))
 
